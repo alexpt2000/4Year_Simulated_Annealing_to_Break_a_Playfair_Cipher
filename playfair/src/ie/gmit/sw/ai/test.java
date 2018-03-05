3c1ae135833835afb06ad1b0ece0db606ae91010
alexpt2000gmit <g00317835@gmit.ie>
2018-03-05 00:07:57 +0000

    Test Runner

--- playfair/src/ie/gmit/sw/ai/test.java
+++ playfair/src/ie/gmit/sw/ai/test.java
@@ -1,7 +1,9 @@
 package ie.gmit.sw.ai;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import ie.gmit.sw.ai.file.Parser;
 import ie.gmit.sw.ai.playfair.Playfair;
@@ -13,8 +15,8 @@ public class test {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
 	
-		// String fileName = "test.txt";
-		String fileName = "x.txt";
+		String fileName = "TheHobbitCypher.txt";
+		// String fileName = "Tips.txt";
 		
 		Parser parser = Parser.getParserFile(new File(fileName));
 		
@@ -36,6 +38,24 @@ public class test {
 		System.out.println("Sample output result.: " + plainText.substring(600, 700) + "...");
 		//System.out.println(plainText.replace("X", ""));
 		System.out.println("------------------------------------------------------------------------------------------");
+		
+		String saveFileName = "_decryptedMSG.txt";
+		String path = "./" + saveFileName;
+
+		try {
+			new FileOutputStream(saveFileName, false).close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		try (PrintWriter pw = new PrintWriter(path)) {
+			// Print into a file
+			pw.println(plainText);
+			System.out.println("Decrypt saved on file name: " + saveFileName);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
